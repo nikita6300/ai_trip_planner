46fdfdf57810fc8a5bbec3aaf09b036f87f55647
nikita6300 <159311986+nikita6300@users.noreply.github.com>
2026-02-24 03:17:48 +0530

    fixed api key

--- src/service/AIModel.jsx
+++ src/service/AIModel.jsx
@@ -10,7 +10,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
   });
   
   const generationConfig = {
